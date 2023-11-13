6d84b0cd5a3d647f9d79caebdc3a92bfc5408ddd
yazan tabak <yazan.tabak@fau.de>
2023-11-13 01:08:26 +0100

    project finalization for pip setup - setup corrected

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='0.1.0',    
+    description='a simple math quiz test package',
+    url='https://github.com/YazanTabak/dsss_homework_2',
+    author='Yazan Tabak',
+    author_email='yazan.tabak@fau.de',
+    license='BSD 2-clause',
+    packages=['math_quiz'],
+    install_requires=[],
+)
